d7914916d603bd598e8df1280371e1ad7f308de9
Ayush-Y23 <ayushmyadav23@gmail.com>
2025-10-25 00:36:38 +0530

    Java code created

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class {
+
+  public static void main(String[] args) {
+       System.out.println(************************************);
+       System.out.println(" Hello From Github ");
+       System.out.println(************************************); 
+  }
+}
